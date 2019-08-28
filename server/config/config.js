a541ccd8c3c3dd14442d620826de4c8c8f8f6e83
higaguin <higaguin@hotmail.com>
2019-08-28 15:51:59 -0500

    almost done with production

--- server/config/config.js
+++ server/config/config.js
@@ -18,9 +18,9 @@ module.exports =
   },
   "production": {
     "username": process.env.POSTGRES_USER,
-    "password": "24a675c34c1d1c5f6c312076b036b6048ec68e399ca6aaa82c7350b5b529b781",
-    "database": "d487ir54gh8ssq",
-    "host": "ec2-184-73-232-93.compute-1.amazonaws.com",
+    "password": process.env.POSTGRES_PASSWORD,
+    "database": process.env.POSTGRES_DATABASE,
+    "host": process.env.POSTGRES_HOST,
     "dialect": "postgres",
     "operatorsAliases": false
   }
