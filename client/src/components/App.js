dce87c2798565ab68b8f3a6c9d9a1d8e2a8537e8
higaguin <higaguin@hotmail.com>
2019-08-29 11:27:46 -0500

    almost done with production

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,34 +1,33 @@
 import React from 'react';
-// import { Router, Route, Switch } from 'react-router-dom';
-// import { InMemoryCache } from 'apollo-cache-inmemory';
-// import { HttpLink } from 'apollo-link-http';
-// import { createBrowserHistory } from 'history';
-// import { ApolloProvider } from 'react-apollo';
-// import ApolloClient from 'apollo-client';
-// import Header from './Header';
-// import Home from './views/Home';
-// import TheaterMovies from './views/TheaterMovies';
-// import FunctionView from './views/FunctionView';
+import { Router, Route, Switch } from 'react-router-dom';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+import { createBrowserHistory } from 'history';
+import { ApolloProvider } from 'react-apollo';
+import ApolloClient from 'apollo-client';
+import Header from './Header';
+import Home from './views/Home';
+import TheaterMovies from './views/TheaterMovies';
+import FunctionView from './views/FunctionView';
 
-// import MovieInfo from './movies/MovieInfo';
+import MovieInfo from './movies/MovieInfo';
 
-// const history = createBrowserHistory()
+const history = createBrowserHistory()
 
-// const cache = new InMemoryCache();
-// const link = new HttpLink({
-//   uri: '/api/graphql'
-// });
+const cache = new InMemoryCache();
+const link = new HttpLink({
+  uri: '/api/graphql'
+});
 
-// const client = new ApolloClient({
-//   cache: cache,
-//   link: link,
-// });
+const client = new ApolloClient({
+  cache: cache,
+  link: link,
+});
 
 const App = () => {
   return (
     <div>
-      miaus
-      {/* <ApolloProvider client={client}>
+      <ApolloProvider client={client}>
         <Router history={history}>
           <div>
             <Header></Header>
@@ -36,10 +35,11 @@ const App = () => {
                 <Route path="/" exact component={Home} />
                 <Route path="/theater-movies/:id" component={TheaterMovies} />
                 <Route path="/function/:id" component={FunctionView} />
+                {/* <Route path="/movies/:id" component={MovieInfo} /> */}
             </Switch>
           </div>
         </Router>
-      </ApolloProvider> */}
+      </ApolloProvider>
     </div>);
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import { Provider } from 'react-redux';
-// import { createStore, applyMiddleware, compose } from 'redux';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import App from './components/App';
-// import reducers from './reducers';
+import reducers from './reducers';
 
 // const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-// const store = createStore(reducers, composeEnhancers());
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers());
 
-// ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
